42235b8ed7af4db78e48ad754aca358740809424
Lucas Kitsuta Sabino <lucasksabino@gmail.com>
2025-09-16 13:07:24 -0300

    Add files via upload
    Removi int atendimento, já que estamos salvando os pets já atendidos, em uma fila própria
    Coloquei espaços para cada função que ainda precisaríamos fazer

---
+++ FILA.h
@@ -0,0 +1,171 @@
+#ifndef FILA_H_INCLUDED
+#define FILA_H_INCLUDED
+
+#include <stdio.h>   // printf
+#include <stdlib.h>  // malloc, free, exit, NULL
+#include <string.h>
+
+/* FUNÇÕES DE MANIPULAÇÃO DE FILA
+
+Fila* CriaFila()  CRIA A FILA
+
+int VaziaFila (Fila* f) VERIFICA SE A FILA EST� VAZIA RETORNA 1 QUANDO EST� VAZIA
+
+void InsereFila (Fila* f, Pet v) INSERIR ELEMENTO
+
+int RetiraFila (Fila* f) REMOVER ELEMENTO
+
+Fila* liberaFila (Fila* f) LIBERA A FILA
+
+void imprimeFila (Fila* f)IMPRIME A FILA
+*/
+
+//---------------------------------------------------------------
+//Definindo o tipo pet
+typedef struct dataNascimento{
+    int dia, mes, ano;
+} data;
+
+typedef struct animal{
+    int id;                         // 100..999, único
+    char nome[50];
+    char especie[30];
+    int idade;
+    data nascimento;
+    int prioridade;                 // 0 = emergência, 1 = normal
+} Pet;
+
+//---------------------------------------------------------------
+//Funções novas para manipulação de Pets
+//---------------------------------------------------------------
+
+// Função para criar um Pet padrão PARA TESTES
+/*
+Pet criaPet(int id, const char* nome, const char* especie, int idade) {
+    Pet p;
+    p.id = id;
+    strcpy(p.nome, nome);
+    strcpy(p.especie, especie);
+    p.idade = 0;
+    p.nascimento.dia = 1;
+    p.nascimento.mes = 1;
+    p.nascimento.ano = 2020;
+    p.prioridade = 1;
+    return p;
+} */
+//----------------------------------------------------------------
+
+
+typedef struct nos
+{
+    Pet info;  // Agora um no guarda um tipo pet
+    struct nos *prox;
+}Nos;
+
+typedef struct fila
+{
+    Nos * ini;
+    Nos * fim;
+} Fila;
+
+int VaziaFila (Fila* f)
+{
+    if (f->ini==NULL) return 1; //A fila ta vazia
+    return 0; //A fila nao ta vazia
+
+}
+
+
+Fila* CriaFila () //o que sera retornado da função sera um ponteiro para um fila
+{
+    Fila* f = (Fila*) malloc(sizeof(Fila));// cria um ponteiro para fila f e aloca um tamanho de memoria no tamanho de uma fila
+    //quando eu uso malloc, um ponteiro genérico é criado, e precisamos especificar para o tipo de dado que ele está apontando
+    //por isso usamos (Fila*)
+    f->ini = f->fim = NULL; // a fila esta sem conteudo
+    return f; //retorna a fila
+}
+
+Nos* ins_fim (Nos *fim, Pet A) //retorna um ponteiro para o novo nó inserido no final. por exemplo:    f->fim = ins_fim(f->fim, 20);
+{
+    Nos *p = (Nos*)malloc(sizeof(Nos)); //cria um ponteiro para o nó, e esse ponteiro se chama p
+    p->info = A;        //Insere as informações do pet no nó
+    p->prox = NULL;     //Como esse sera o final da fila, nao tera proximo
+    if (fim != NULL){   //fim é um ponteiro que aponta para um nó, definindo assim o nó fim. Portanto se ele for null
+                        //significa que ele nao esta apontando para ninguem
+        fim->prox = p;  // o nó apontado por fim tem a parte prox, que é um ponteiro para outro nó, nesse ponteiro tera o endereço de p
+    }
+    return p;
+
+}
+
+void InsereFila (Fila* f, Pet v)
+{
+    f->fim = ins_fim(f->fim,v);
+    if (f->ini==NULL){ /* se a fila estava vazia, agora ambos o início e o fim apontam para o mesmo no*/
+        f->ini = f->fim;
+    }
+}
+
+Nos* retira_ini (Nos* ini)
+{
+    Nos* p = ini->prox;
+    free(ini);
+    return p;
+}
+
+Pet RetiraFila (Fila* f)
+{
+    Pet v;
+    if (VaziaFila(f))
+    {
+        printf("Fila vazia.\n");
+        exit(0); /* aborta programa */
+    }
+    v = f->ini->info;                   //guarda as informações do pet no início da fila
+    f->ini = retira_ini(f->ini);        //retira o item do início da fila e avança o f->ini para o próximo item
+    if (f->ini == NULL)                 // fila ficou vazia?
+    f->fim = NULL;
+    return v;
+}
+//---------------------------------------------------------
+//Função para imprimir os pets na fila
+//---------------------------------------------------------
+void imprimeFila(Fila* f) {
+    Nos* q;
+    printf("\n");
+    printf("%-5s | %-15s | %-15s | %-5s | %-15s | %s\n",
+           "ID", "Nome", "Especie", "Idade","Prioridade", "Data de nascimento");
+    printf("---------------------------------------------------------------\n");
+
+    for (q = f->ini; q != NULL; q = q->prox) {
+        printf("%-5d | %-15s | %-15s | %-5d | %-15s | %02d/%02d/%04d \n", // <-- Alteração aqui
+               q->info.id,
+               q->info.nome,
+               q->info.especie,
+               q->info.idade,
+               (q->info.prioridade == 0 ? "Emergencial(0)" : "Normal(1)"),
+               q->info.nascimento.dia, q->info.nascimento.mes, q->info.nascimento.ano
+        );
+    }
+    printf("\n");
+}
+
+
+Fila* liberaFila (Fila* f)
+{
+    Nos* q = f->ini;
+    while (q!=NULL)
+    {
+        Nos* t = q->prox;
+        free(q);
+        q = t;
+    }
+    free(f);
+    return NULL;
+}
+
+
+
+
+
+#endif // FILA_H_INCLUDED
---
+++ clinica_veterinaria.c
@@ -0,0 +1,230 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "FILA.h"
+
+// tipo Pet e caracteristicas
+/*
+typedef struct animal{
+    int id;                  // entre 100 e 999
+    char nome[50];
+    char especie[30];
+    int idade;
+    data nascimento{int dia, mes, ano;};
+    int prioridade;          // 0 = emergência, 1 = normal
+} Pet;
+*/
+
+// --------------------------------------------------------
+// Menu
+// --------------------------------------------------------
+int mostrarMenu() {
+
+    int escolha;
+    printf("\n==============================================\n");
+    printf("   CLINICA VETERINARIA - GERENCIAMENTO DE FILAS\n");
+    printf("==============================================\n");
+    printf("1) Inserir pet na fila\n");
+    printf("2) Atender proximo pet\n");
+    printf("3) Buscar pet por Nome/ID\n");
+    printf("4) Imprimir relatorio das filas\n");
+    printf("5) Mostrar proximo a ser atendido\n");
+    printf("6) Listar pets ja atendidos\n");
+    printf("7) Finalizar\n");
+    printf("----------------------------------------------\n");
+
+    printf("Escolha: ");
+    fflush(stdout);
+    scanf("%d", &escolha);
+    getchar(); // consome o '\n' deixado no buffer
+    return escolha;
+}
+
+
+
+
+//codigo para testar o funcionamento
+/*int main() {
+    Fila* f = CriaFila();
+
+    // Inserindo alguns pets
+    InsereFila(f, criaPet(101, "Rex", "Cachorro", 5));
+    InsereFila(f, criaPet(202, "Mimi", "Gato", 2));
+    InsereFila(f, criaPet(303, "Bob", "Coelho", 4));
+
+    printf("Fila inicial:");
+    imprimeFila(f);
+
+    // Retirando um pet e mostrando
+    Pet p = RetiraFila(f);
+    printf("\nPet atendido: %s (ID:%d)\n", p.nome, p.id);
+
+    printf("\nFila após atendimento:");
+    imprimeFila(f);
+
+    f = liberaFila(f);
+    return 0;
+}
+*/
+//===============================================
+//Funções principais
+//===============================================
+
+
+//Gera um novo id que ainda nao foi utilizado
+int gerarID(){
+    int id = rand() % 900 + 100; // 100 a 999
+    return id;
+}
+
+
+//Cria um novo pet
+Pet criaPet(){
+    Pet p;
+
+    p.id = gerarID();
+
+    printf("Nome: ");
+    fgets(p.nome, sizeof(p.nome), stdin);
+    p.nome[strcspn(p.nome, "\n")] = '\0';
+
+
+    printf("Especie: ");
+    fgets(p.especie, sizeof(p.especie), stdin);
+    p.especie[strcspn(p.especie, "\n")] = '\0';
+
+    printf("Idade: ");
+    scanf("%d", &p.idade);
+    getchar(); // limpa o \n deixado no buffer
+
+    printf("Ano de nascimento: ");
+    scanf("%d", &p.nascimento.ano);
+    getchar();
+
+    printf("Mes de nascimento: ");
+    scanf("%d", &p.nascimento.mes);
+    getchar();
+
+    printf("Dia de nascimento: ");
+    scanf("%d", &p.nascimento.dia);
+    getchar();
+
+    printf("Tipo de prioridade (0 = Emergencia | 1 = Normal ): ");
+    scanf("%d", &p.prioridade);
+    getchar();
+
+    return p;
+}
+
+//Insere um novo pet na fila
+
+void inserirPet(Fila *f0, Fila *f1){
+
+    //cria o pet
+    Pet novoPet = criaPet();
+
+    //insere o pet na fila
+    if (novoPet.prioridade == 0){
+        InsereFila(f0, novoPet); // Insere na fila emergencial
+    } else{
+        InsereFila(f1, novoPet); // Insere na fila normal
+    }
+}
+
+/*void atenderPet(){
+
+    FAZER COMANDO PARA ATENDER UM PET E RETIRÁ-LO DA FILA DE ATENDIMENTO
+        - VER SE HÁ PET NA FILA DE ESPERA, SENÃO, ATENDER DA FILA NORMAL
+        - PETS RETIRADOS DA FILA DE ATENDIMENTO DEVEM SER INSERIDOS NA FILA DE ATENDIDOS
+
+}
+*/
+
+/*void buscarPet(){
+
+    FAZER COMANDO PARA BUSCAR UM PET POR ID OU POR NOME E CASO SEJA ENCONTRADO, IMPRIMIR SUAS INFORMAÇÕES, OU RETORNAR O OBJETO PET
+
+}
+*/
+
+/*void imprimirAtendimento(){
+
+    IMPRIMIR AS DUAS FILAS DE ESPERA
+}
+*/
+
+/*void imprimirProximo(){
+
+    IMPRIMIR O PRÓXIMO PET A SER ATENDIDO
+}
+*/
+
+/*void imprimirAtendidos(){
+
+    IMPRIMIR TODOS OS PETS JÁ ATENDIDOS
+}
+*/
+
+int main() {
+
+    srand(time(NULL)); // semente para o programa inteiro
+
+ // CRIANDO FILAS
+
+    Fila *filaEmergencia = CriaFila();
+    Fila *filaNormal     = CriaFila();
+    Fila *filaAtendidos  = CriaFila();
+    int numEscolha;
+
+    do {
+        numEscolha = mostrarMenu();
+
+        switch (numEscolha) {
+            case 1:
+
+                inserirPet(filaEmergencia, filaNormal);
+                break;
+
+            case 2:
+                // Atender próximo
+                //atenderPet(filaEmergencia, filaNormal, filaAtendidos);
+                break;
+
+            case 3:
+                // Buscar por nome ou ID
+                //buscarPet(filaEmergencia, filaNormal, filaAtendidos);
+                break;
+
+            case 4:
+                // Imprimir relatório das filas
+                //imprimeRelatorio(filaEmergencia, filaNormal);
+                break;
+
+            case 5:
+                // Mostrar próximo da fila
+                //mostrarProximo(filaEmergencia, filaNormal);
+                break;
+
+            case 6:
+                // Listar pets já atendidos
+                //imprimeFila(filaEmergencia);
+                break;
+
+            case 7:
+                printf("\nFinalizando o sistema.\n");
+                break;
+
+            default:
+                printf("\nDigite um numero valido!\n");
+                break;
+        }
+
+    } while (numEscolha != 7);
+
+    // Libera memoria e deixa os ponteiros null pois o libera fila retorna null
+    filaEmergencia = liberaFila(filaEmergencia);
+    filaNormal     = liberaFila(filaNormal);
+    filaAtendidos  = liberaFila(filaAtendidos);
+
+    return 0;
+}
